32f2e1bd3b5d940c83f8b88a533d692357dc892f
Keita <kitswd11@gmail.com>
2017-11-22 21:19:18 +0900

    シーンネームクラスを作成

---
+++ Assets/Scripts/Common/SceneName.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneName
+{
+    public const string Title    = "Title";
+    public const string WorldMap = "WorldMap";
+    public const string Base     = "Base";
+    public const string Battle   = "Battle";
+}
