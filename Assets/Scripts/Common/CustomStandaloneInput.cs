5408502739ce46e27455ad8ba01c4be2d0f67a95
hironari ushiyama <lovemika9528202605@yahoo.co.jp>
2017-11-25 19:33:29 +0900

    Cursor 簡易コントロール機能の追加

--- Assets/Scripts/Common/CustomStandaloneInput.cs
+++ Assets/Scripts/Common/CustomStandaloneInput.cs
@@ -35,4 +35,26 @@ public class CustomStandaloneInput : StandaloneInputModule
             ProcessMouseEvent();
         }
     }
+
+	/*===============================================================*/
+	/// <summary>マウスコントロール</summary>
+	/// <remarks>
+	/// 1を選択するとプレイモード中にマウスでの停止が出来なくなります
+	/// DebugDisplayLog.csをカメラなどのオブジェクトにアタッチした状態でESCキーを押すことで停止できます
+	/// (注意)1を選択してもマウスイベントは取得されます:中央にロックされるのみです
+	/// </remarks>
+	/// <param name="control">
+	/// 1:マウスカーソルを中央にロックします
+	/// 2:ウィンドウ内のみマウスカーソルを有効にします
+	/// 3:マウスカーソルを自由に動かせるようにします
+	/// </param>
+	static public void MouseEnable( int control ) {
+		if( control == 1 ) Cursor.lockState = CursorLockMode.Locked;
+		else if( control == 2 ) Cursor.lockState = CursorLockMode.Confined;
+		else if( control == 3 ) Cursor.lockState = CursorLockMode.None;
+		else Debug.LogError( "代入値が範囲外です！確認して下さい！" );
+
+
+	}
+	/*===============================================================*/
 }
