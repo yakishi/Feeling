63814396650a822fc6c327f528ee1d858438013b
Terada <shin,yakishi222@gmail.com>
2018-01-26 12:40:07 +0900

    ワールドマップシーンの修正

---
+++ Assets/Scripts/SelectText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    static public void Select(GameObject button)
+    {
+        Text text = button.GetComponent<Text>();
+        text.color = Color.red;
+    }
+
+    static public void DeSelect(GameObject button)
+    {
+        Text text = button.GetComponent<Text>();
+        if (text != null)
+            text.color = Color.white;
+    }
+}
--- Assets/Scripts/WorldMap/WorldMapUI.cs
+++ Assets/Scripts/WorldMap/WorldMapUI.cs
@@ -1,121 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
 
 public class WorldMapUI : MonoBehaviour {
 
-    private GameObject[] dungeons = new GameObject[6];
-    private GameObject[] destinationList = new GameObject[7];
+    [SerializeField]
+    private Button[] destinationList = new Button[7];
+    [SerializeField]
+    private Image[] dungeonRedPointArry = new Image[6];
+    [SerializeField]
+    GameObject listGrid;
     
+    [SerializeField]
     private GameObject dungeonDetail;
-    private GameObject[] goOrBack = new GameObject[2];
+    [SerializeField]
+    private Button[] goOrBack = new Button[2];
+    [SerializeField]
+    GameObject detailGrid;
 
-    private Vector3 defScale;
-    private Vector3 choiceMap;
+    List<GameObject> beforeSelect;
 
-    int goBack;
-    int choiceElement;
-    private bool isOpen;
-
-    public bool IsOpen
-    {
-        get
-        {
-            return isOpen;
-        }
-        set
-        {
-            isOpen = value;
-        }
-    }
+    Vector3 defScale;
 
     // Use this for initialization
     void Start () {
-		for(int i = 0;i < dungeons.Length;i++) {
-            dungeons[i] = GameObject.Find("Dungeon" + (i + 1));
-            destinationList[i] = GameObject.Find("DungeonList" + (i + 1));
-        }
+        beforeSelect = new List<GameObject>();
+        defScale = new Vector3(0.5f, 0.5f, 1.0f);
 
-        destinationList[6] = GameObject.Find("Base");
-
-        dungeonDetail = GameObject.Find("Detail");
-
-        goOrBack[0] = GameObject.Find("Go");
-        goOrBack[1] = GameObject.Find("Back");
-
-        defScale = dungeons[0].transform.localScale;
-        choiceMap = defScale * 1.4f;
-
-        choiceElement = 0;
-        goBack = 0;
-        isOpen = false;
+        ButtonsObservableInitialize();
 
         dungeonDetail.SetActive(false);
+        BattleUI.ActiveButton(listGrid);
 	}
+
+    void ButtonsObservableInitialize()
+    {
+        foreach(Button b in destinationList) {
+            b.OnClickAsObservable()
+                .Subscribe(_ => {
+                    dungeonDetail.SetActive(true);
+                    BattleUI.NotActiveButton(listGrid);
+                    BattleUI.ActiveButton(detailGrid);
+
+                    beforeSelect.Add(b.gameObject);
+                })
+                .AddTo(this);
+        }
+
+        goOrBack[0].OnClickAsObservable()
+            .Subscribe(_ => {
+                //ダンジョンへのシーン遷移
+
+                beforeSelect.Clear();
+            })
+            .AddTo(this);
+
+        goOrBack[1].OnClickAsObservable()
+            .Subscribe(_ => {
+                BattleUI.NotActiveButton(detailGrid);
+                BattleUI.ActiveButton(listGrid,beforeSelect[beforeSelect.Count - 1]);
+                dungeonDetail.SetActive(false);
+
+                beforeSelect.Clear();
+            })
+            .AddTo(this);
+
+    }
 
     // Update is called once per frame
     void Update () {
-        if (isOpen) {
-            Detail();
-            return;
-        }
-        
-
-        if (MyInput.isButtonDown()) {
-            choiceElement -= (int)MyInput.direction().y;
-
-            if (choiceElement < 0) {
-                choiceElement = 0;
-            }
-            if (choiceElement >= destinationList.Length) {
-                choiceElement = destinationList.Length - 1;
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return)) {
-            dungeonDetail.SetActive(true);
-            
-            isOpen = true;
-        }
-
-        ChoiceMap();
     }
 
-    void Detail()
+
+    public void EnlargementDungeon(int i)
     {
-
-        if (MyInput.isButtonDown()) {
-            goBack += (int)MyInput.direction(false).x;
-
-            if (goBack < 0) {
-                goBack = 0;
-            }
-            if (goBack >= goOrBack.Length) {
-                goBack = goOrBack.Length - 1;
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return)) {
-
-            if (goBack == 1) {
-                dungeonDetail.SetActive(false);
-                isOpen = false;
-                goBack = 0;
-            }
-        }
+        dungeonRedPointArry[i].transform.localScale = new Vector3(1.2f, 1.2f, 1.0f);
     }
 
-    void ChoiceMap()
+    public void ReductionDungeon(int i)
     {
-        if (choiceElement == destinationList.Length - 1) return;
-
-        dungeons[choiceElement].transform.localScale = choiceMap;
-
-        for(int i = 0; i < dungeons.Length; i++) {
-            if(i != choiceElement) {
-                dungeons[i].transform.localScale = defScale;
-            }
-        }
+        dungeonRedPointArry[i].transform.localScale = defScale;
     }
-
 }
